fec0f375feb33b92500c3d5264f63bfcd1488216
Josef Klafka <jklafka@uchicago.edu>
2018-05-06 22:46:52 -0500

    Fixed steps, how to carry word_dict over to second reducer

--- part1.py
+++ part1.py
@@ -6,46 +6,49 @@ import heapq
 
 class FindAllButTop50(MRJob):
 
-	def mapper_make_heap(self, _, line):
-		review = json.loads(line)
-		words = re.findall(r'[a-zA-Z]+', review['reviewText'])
-		for word in words:
-			yield word.lower(), 1
+    def mapper_make_heap(self, _, line):
+        review = json.loads(line)
+        words = re.findall(r'[a-zA-Z]+', review['reviewText'])
+        for word in words:
+            yield word.lower(), 1
 
 
-	def combiner_make_heap(self, word, counts):
-		yield word, sum(counts)
+    def combiner_make_heap(self, word, counts):
+        yield word, sum(counts)
 
 
-	def reducer_make_heap_init(self):
-		self.heap = [(0, [])] * 40
-		heapq.heapify(self.heap)
-		print("made heap")
+    def reducer_make_heap_init(self):
+        self.heap = [(0, [])] * 40
+        self.word_dict = {}
+        heapq.heapify(self.heap)
 
-	def reducer_make_heap(self, word, counts):
-		summ = sum(counts)
-		if summ > self.heap[0][0]:
-			heapq.heapreplace(self.heap, (summ, word))
-		yield word, summ
+    def reducer_make_heap(self, word, counts):
+        summ = sum(counts)
+        if summ > self.heap[0][0]:
+            heapq.heapreplace(self.heap, (summ, word))
+        yield word, summ
 
-	def reducer_make_heap_final(self):
-		self.word_dict = {}
-		for entry in self.heap:
-			self.word_dict[entry[1]] = None
-
-
-	def reducer_filter_words(self, word, counts):
-		if word not in self.word_dict:
-			yield word
+    def reducer_make_heap_final(self):
+        for entry in self.heap:
+            self.word_dict[entry[1]] = None
 
 
-	def steps(self):
-		return [
-			MRStep(mapper=self.mapper_make_heap,
-					combiner=self.combiner_make_heap, 
-					reducer=self.reducer_make_heap),
-			MRStep(reducer=self.reducer_filter_words)
-			]
+    def reducer_filter_words(self, word, counts):
+        print("final step")
+        print(self.heap is None)
+        if word not in self.word_dict:
+            yield word
+
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_make_heap,
+                    combiner=self.combiner_make_heap, 
+                    reducer_init=self.reducer_make_heap_init,
+                    reducer=self.reducer_make_heap,
+                    reducer_final=self.reducer_make_heap_final),
+            MRStep(reducer=self.reducer_filter_words)
+            ]
 
 if __name__ == '__main__':
-	FindAllButTop50.run()
+    FindAllButTop50.run()
